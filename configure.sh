c2c4c0b6dd9dd625b0a50ee7844b77a541c80b91
Luke Roberts <luke.roberts.15@ucl.ac.uk>
2017-07-30 09:35:22 +0100

    [Meta] Forces update of hardlinks.

--- configure.sh
+++ configure.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 mkdir -p ~/.config/i3
-ln ~/.dotfiles/i3/config ~/.config/i3/config
+ln -f ~/.dotfiles/i3/config ~/.config/i3/config
 
-ln ~/.dotfiles/i3blocks/i3blocks.conf ~/.i3blocks.conf
+ln -f ~/.dotfiles/i3blocks/i3blocks.conf ~/.i3blocks.conf
 
-ln ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -f ~/.dotfiles/vim/vimrc ~/.vimrc
 
-ln ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -f ~/.dotfiles/zsh/zshrc ~/.zshrc
 
-ln ~/.dotfiles/rofi/Xresources ~/.Xresources
+ln -f ~/.dotfiles/rofi/Xresources ~/.Xresources
 
 mkdir -p ~/.config/dunst
-ln ~/.dotfiles/dunst/config ~/.config/dunst/config
+ln -f ~/.dotfiles/dunst/config ~/.config/dunst/config
 
-ln ~/.dotfiles/xinit/xinitrc ~/.xinitrc
+ln -f ~/.dotfiles/xinit/xinitrc ~/.xinitrc
